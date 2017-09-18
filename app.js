b450d7565d6329a16735592b0275fcea6eb74064
apellanr <ryan.apellanes@gmail.com>
2017-09-17 22:53:01 -0700

    card appending to the dom

--- app.js
+++ app.js
@@ -10,7 +10,6 @@ TO DO LIST:
 - On desktop, the number of columns should grow to fill the full browser width if user resizes.
 - On mobile, please only show a single column of movie posters and titles at all times.
 
-
 Look up documentation of movieDB API - which queries to target
 URL link: https://developers.themoviedb.org/3/search/search-movies
 
@@ -26,27 +25,74 @@ $(document).ready(function(){
     handleEvents();
 });
 
+var movieData = null,
+    url = 'https://api.themoviedb.org/3/search/movie?query=',
+    key = '&api_key=51a0abce642402e7b8d43b2081302e77'
+    imgBasePath = 'http://image.tmdb.org/t/p/w154',
+    noImgUrl = 'http://lnbnews.com/wp-content/uploads/2017/04/org_default.jpg';
+
 function handleEvents() {
     $(".search").on('click', function(){
         console.log('submit clicked');
-        getMovieData();
-    })
+        var input = $(".input").val();
+        getMovieData(input);
+    });
 }
 
-function getMovieData() {
+/* --------------------------
+MOVIEDB CALL
+----------------------------*/
+function getMovieData(input) {
     console.log('calling moviedb');
       $.ajax({
         "async": true,
         "crossDomain": true,
-        "url": "https://api.themoviedb.org/3/search/movie?include_adult=false&page=1&query=Troll&language=en-US&api_key=51a0abce642402e7b8d43b2081302e77",
-        "method": "GET",
-        "headers": {},
-        "data": "{}"
+        "url": url + input + key,
+        "method": "get",
       })
       .done(function(response){
-        console.log(response);
-      });
+          movieData = response.results;
+          console.log(movieData);
+
+          for(var i = 0; i < movieData.length; i++) {
+              if(movieData.poster_path === null) {
+                var moviePosterImg = noImgUrl;
+                console.log(noImgUrl);
+              } else {
+                var posterPath = movieData[i].poster_path;
+                var filmTitle = movieData[i].title;
+                moviePosterImg = imgBasePath + posterPath;
+              }
+              
+            //   console.log(moviePosterImg, filmTitle);
+              createCardElements(moviePosterImg, filmTitle);
+          }
+          
+    });
 }
+
+function createCardElements(img, title) {
+    // Dom creation of card top
+    var $div = $("<div>").addClass("col-sm-6 col-md-4 col-lg-3 mt-4");
+    var $cardDiv = $("<div>").addClass("card");
+    var $img = $("<img>").addClass("card-img-top").attr("src", img);
+    $cardDiv.append($img);
+
+    // Dom creation of card block
+    var $cardBlock = $("<div>").addClass("card-block");
+    var $cardText = $("<p>").text(title);
+    $cardBlock.append($cardText);
+
+    // Append to card-row div in html page
+    $div.append($cardDiv, $cardBlock);
+    displayData($div);
+}
+
+function displayData(movieInfo) {
+    $(".card-row").append(movieInfo);
+}
+
+
 
 // TEST API in postman
 // http://api.themoviedb.org/3/search/movie?api_key=51a0abce642402e7b8d43b2081302e77&query=Troll
